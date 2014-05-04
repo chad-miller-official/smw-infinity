253fbd72c437b19011acbb96a96f6ad44e6537f9
eon8ight <everynamewastaken123@gmail.com>
2014-05-04 18:12:32 -0400

    Restarting
    I can't for the life of me figure out why nothing was drawing correctly,
    and neither can anyone I talked to.

--- src/smw/infinity/RenderStrategy.java
+++
@@ -1,8 +0,0 @@
-package smw.infinity;
-
-import java.awt.Graphics;
-
-public interface RenderStrategy
-{
-	void render(Graphics g);
-}
--- src/smw/infinity/Updatable.java
+++
@@ -1,6 +0,0 @@
-package smw.infinity;
-
-public interface Updatable
-{
-	void update(long timePassed);
-}
